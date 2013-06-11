f9d02e4d7f15d72276c7addb5b50ddf52cc1dd7e
Raphaël Jacquot <sxpert@sxpert.org>
2013-06-11 07:32:31 +0200

    modify comments refactor packet logging features

--- myhome.js
+++ myhome.js
@@ -6,6 +6,9 @@ var openpass = require('./openpass');
 var MODE_MONITOR = 0;
 var MODE_COMMAND = 1;
 var MODE_STR = [ 'MON', 'CMD' ];
+var DIR_IN = 0;
+var DIR_OUT = 1;
+var DIR_STR = ['<=','=>'];
 
 var STATE_UNCONNECTED = 0;
 var STATE_CONNECTING = 1;
@@ -16,10 +19,11 @@ var PKT_NACK = '*#*0##';
 var PKT_START_COMMAND = '*99*9##';
 var PKT_START_MONITOR = '*99*1##';
 
-/******************************************************************************
- *
- * handles the lower layer of the connection
- *
+//=============================================================================
+
+/* layer 1 of the connection th OpenWebNet
+ * handles all the details of logging in the gateway
+ * then, starts returning all packets to parent object
  */ 
 
 var myHomeConn = function (params) {
@@ -27,14 +31,30 @@ var myHomeConn = function (params) {
 	var self = this;
 	var mode = params.mode;
 	var state = STATE_UNCONNECTED;
+	
+	function logPacket (direction, packet) {
+		var now = new Date();
 		
+		console.log (now.toISOString()+' - '+MODE_STR[mode]+' '+DIR_STR[direction]+' '+packet);		
+	}
+	
+	/*
+	 * send a packet through the connection
+	 */
 	this.sendPacket = function (pkt) {
-		console.log (MODE_STR[mode]+' -> '+pkt);
+		logPacket(DIR_OUT, pkt);
 		conn.write (pkt);
 	};
+	
+	/*
+	 * parse the basics of the incoming packets
+	 */
 	this.parseMyOpenPacket = function (data) {
 		var sdata = data.toString();
-		console.log(MODE_STR[mode]+' <- '+state+' - \''+sdata+'\'');
+		
+		/* TODO: handle the fact that more that one packet can come in at the same time */
+		
+		logPacket (DIR_IN, sdata);
 		switch (state) {
 			case STATE_UNCONNECTED:
 				/* initial ack from gateway */
@@ -54,6 +74,7 @@ var myHomeConn = function (params) {
 			case STATE_CONNECTING:
 				if (sdata==PKT_ACK) {
 					/* connected already */
+					/* state should be STATE_CONNECTED at this point, I guess */
 				} else {
 					/* probably need to login */
 					/* login nonce is of the form *#<numbers>## */
@@ -84,22 +105,25 @@ var myHomeConn = function (params) {
 				break;
 		}
 	};
+	
+	/*
+	 * half-closes the connection.
+	 * server can still send stuff that will get received 
+	 */
 	this.end = function () {
 		conn.end();
 	}
 	
 	/* TODO: catch EADDRNOTAVAIL - device not present */
-	var conn = net.connect(params, function () {
-		console.log ('client connected');
-	});
+	var conn = net.connect(params);
 	conn.on ('data', self.parseMyOpenPacket);	
 };
 util.inherits(myHomeConn, events.EventEmitter);	
 
-/******************************************************************************
- *
- * handles the layer 2 of the connection
- *
+//=============================================================================
+
+/* layer 2 of the connection to myhome
+ * starts initially with a monitor connection
  */ 
 
 var myHomeEngine = function (params) {
@@ -112,7 +136,6 @@ var myHomeEngine = function (params) {
 	 * params.done	  : callback for when we're at the end
 	 */
 	this.sendCommand = function (params) {
-		console.log ('requesting system status');
 		/* connects to the same device as the parent connection */
 		var connparams = {
 			"host": host,
@@ -123,7 +146,6 @@ var myHomeEngine = function (params) {
 		
 		var commandconn = new myHomeConn (connparams);
 		commandconn.on ('connected', function () {	
-			console.log ('connected in command mode');
 			commandconn.sendPacket (params.command);
 		});
 		commandconn.on ('packet', function (data) {
@@ -133,8 +155,10 @@ var myHomeEngine = function (params) {
 				params.packet(data);
 		});
 	};
+	
 	var self = this;
-	console.log ('myhome module starting');
+	
+	/* check parameters */
 	var host;
 	var port;
 	var pass;
@@ -143,6 +167,8 @@ var myHomeEngine = function (params) {
 		port = params.port;
 		pass = params.pass;
 	}
+
+	/* default values */
 	if (host===undefined)
 		host = '192.168.0.35';
 	if (port===undefined) 
@@ -150,26 +176,25 @@ var myHomeEngine = function (params) {
 	if (pass===undefined)
 		pass = '12345';
 	
-	console.log (host);
-	console.log (port);
+	/* set up connection parameters */
 	params = {
 		"host": host,
 		"port": port,
 		"pass": pass,
 		"mode": MODE_MONITOR
 	};
+	
+	/* start the connection to the myhome gateway */	
 	var monitor = new myHomeConn (params);
-	monitor.on('connecting', function () {
-		console.log ('connecting to gateway');
-	});
-	monitor.on('logging-in', function () {
-		console.log ('logging in gateway');	
-	});
 	monitor.on('connected', function () {
 		self.emit ('monitoring');
 	});
 };
 util.inherits(myHomeEngine, events.EventEmitter);	
+
+//=============================================================================
+
+
 
 exports.PKT_ACK = PKT_ACK;
 exports.PKT_NACK = PKT_NACK;
