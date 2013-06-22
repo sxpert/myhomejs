fdf5ae3a7dd1251d124f78e946523fc2ac2e2000
Raphaël Jacquot <sxpert@sxpert.org>
2013-06-22 10:01:18 +0200

    major update *	modified:   myhome.js 	- rename the objects internally 	- fix the packet receiver to handle multiple packets per read 	- add sending a command, and waiting for a particular end packet 	- add a log parameter, so as to log or not the incoming packets 		at the lower layers level, to help in debugging

--- myhome.js
+++ myhome.js
@@ -21,21 +21,23 @@ var PKT_START_MONITOR = '*99*1##';
 
 //=============================================================================
 
-/* layer 1 of the connection th OpenWebNet
+/* layer 1 of the connection to OpenWebNet
  * handles all the details of logging in the gateway
  * then, starts returning all packets to parent object
  */ 
 
-var myHomeConn = function (params) {
+var myHomeLayer1 = function (params) {
 	events.EventEmitter.call (this);
 	var self = this;
 	var mode = params.mode;
+	var log = params.log;
 	var state = STATE_UNCONNECTED;
 	
 	function logPacket (direction, packet) {
-		var now = new Date();
-		
-		console.log (now.toISOString()+' - '+MODE_STR[mode]+' '+DIR_STR[direction]+' '+packet);		
+		if (log) {
+			var now = new Date();
+			console.log (now.toISOString()+' - '+MODE_STR[mode]+' '+DIR_STR[direction]+' '+packet);
+		}
 	}
 	
 	/*
@@ -52,57 +54,63 @@ var myHomeConn = function (params) {
 	this.parseMyOpenPacket = function (data) {
 		var sdata = data.toString();
 		
-		/* TODO: handle the fact that more that one packet can come in at the same time */
-		
-		logPacket (DIR_IN, sdata);
-		switch (state) {
-			case STATE_UNCONNECTED:
-				/* initial ack from gateway */
-				if (sdata==PKT_ACK) {
-					self.emit ('connecting');
-					state = STATE_CONNECTING;
-					switch (mode) {
-						case MODE_MONITOR:
-							self.sendPacket (PKT_START_MONITOR);
-							break;
-						case MODE_COMMAND:
-							self.sendPacket (PKT_START_COMMAND);
-							break;
-					}						
-				}
-				break;
-			case STATE_CONNECTING:
-				if (sdata==PKT_ACK) {
-					/* connected already */
-					/* state should be STATE_CONNECTED at this point, I guess */
-				} else {
-					/* probably need to login */
-					/* login nonce is of the form *#<numbers>## */
-					var m = sdata.match(/\*#(\d+)##/);
-					if (m===null) {
-						/* no match ? */
-						console.log ('unable to recognize packet \''+sdata+'\'');
-					} else {	
-						self.emit ('logging-in');
-						/* nonce is first captured string from regexp */
-						var p = openpass.calcPass(params.pass, m[1]);
-						state = STATE_LOGGING_IN;
-						self.sendPacket('*#'+p+'##');
+		/* handle the fact that more that one packet can come in at the same time */
+		while (sdata.length>0) {
+			var m = sdata.match(/(\*.+?##)(.*)/);
+			/* first packet is m[1], rest is m[2] */
+			packet = m[1];
+			sdata = m[2];
+
+			logPacket (DIR_IN, packet);
+			switch (state) {
+				case STATE_UNCONNECTED:
+					/* initial ack from gateway */
+					if (packet==PKT_ACK) {
+						self.emit ('connecting');
+						state = STATE_CONNECTING;
+						switch (mode) {
+							case MODE_MONITOR:
+								self.sendPacket (PKT_START_MONITOR);
+								break;
+							case MODE_COMMAND:
+								self.sendPacket (PKT_START_COMMAND);
+								break;
+						}						
 					}
-				}
-				break;
-			case STATE_LOGGING_IN :
-				if (sdata==PKT_ACK) {
-					self.emit ('connected');
-					state = STATE_CONNECTED;
-				} else {
-					console.log ('got unexpected packet');
-				}
-				break;
-			case STATE_CONNECTED :
-				/* handle packets */
-				self.emit ('packet', sdata);
-				break;
+					break;
+				case STATE_CONNECTING:
+					if (packet==PKT_ACK) {
+						/* connected already */
+						/* state should be STATE_CONNECTED at this point, I guess */
+					} else {
+						/* probably need to login */
+						/* login nonce is of the form *#<numbers>## */
+						m = packet.match(/\*#(\d+)##/);
+						if (m===null) {
+							/* no match ? */
+							console.log ('unable to recognize packet \''+packet+'\'');
+						} else {	
+							self.emit ('logging-in');
+							/* nonce is first captured string from regexp */
+							var p = openpass.calcPass(params.pass, m[1]);
+							state = STATE_LOGGING_IN;
+							self.sendPacket('*#'+p+'##');
+						}
+					}
+					break;
+				case STATE_LOGGING_IN :
+					if (packet==PKT_ACK) {
+						self.emit ('connected');
+						state = STATE_CONNECTED;
+					} else {
+						console.log ('got unexpected packet');
+					}
+					break;
+				case STATE_CONNECTED :
+					/* handle packets */
+					self.emit ('packet', packet);
+					break;
+			}
 		}
 	};
 	
@@ -118,7 +126,7 @@ var myHomeConn = function (params) {
 	var conn = net.connect(params);
 	conn.on ('data', self.parseMyOpenPacket);	
 };
-util.inherits(myHomeConn, events.EventEmitter);	
+util.inherits(myHomeLayer1, events.EventEmitter);	
 
 //=============================================================================
 
@@ -126,7 +134,7 @@ util.inherits(myHomeConn, events.EventEmitter);
  * starts initially with a monitor connection
  */ 
 
-var myHomeEngine = function (params) {
+var myHomeLayer2 = function (params) {
 	events.EventEmitter.call (this);
 	
 	/* the params object contains the following :
@@ -141,16 +149,27 @@ var myHomeEngine = function (params) {
 			"host": host,
 			"port": port,
 			"pass": pass,
-			"mode": MODE_COMMAND
+			"mode": MODE_COMMAND,
+			"log" : log,
 		};
-		
-		var commandconn = new myHomeConn (connparams);
+		var commandconn = new myHomeLayer1 (connparams);
 		commandconn.on ('connected', function () {	
 			commandconn.sendPacket (params.command);
 		});
 		commandconn.on ('packet', function (data) {
+			function end (data, index) {
+				commandconn.end();
+				if (params.end)
+					params.end(data, index);
+			}
 			/* check if data is in stopon variable */
-			commandconn.end();
+			if (params.stopon!==undefined) {
+				if (Array.isArray(params.stopon)) {
+					var i = params.stopon.indexOf(data);
+					if (i!=-1)	return end (data, i);
+				} else if (data==params.stopon) 
+					return end (data, 0);
+			} 
 			if (params.packet) 
 				params.packet(data);
 		});
@@ -162,10 +181,12 @@ var myHomeEngine = function (params) {
 	var host;
 	var port;
 	var pass;
+	var log;
 	if (params) {
 		host = params.host;
 		port = params.port;
 		pass = params.pass;
+		log  = params.log;
 	}
 
 	/* default values */
@@ -175,27 +196,36 @@ var myHomeEngine = function (params) {
 		port = 20000;
 	if (pass===undefined)
 		pass = '12345';
+	if (log===undefined)
+		log = true;
 	
 	/* set up connection parameters */
 	params = {
 		"host": host,
 		"port": port,
 		"pass": pass,
-		"mode": MODE_MONITOR
+		"mode": MODE_MONITOR,
+		"log" : log,
 	};
 	
 	/* start the connection to the myhome gateway */	
-	var monitor = new myHomeConn (params);
+	var monitor = new myHomeLayer1 (params);
 	monitor.on('connected', function () {
 		self.emit ('monitoring');
 	});
+	monitor.on('packet', function (data) {
+		self.emit ('packet', data);
+	});
 };
-util.inherits(myHomeEngine, events.EventEmitter);	
+util.inherits(myHomeLayer2, events.EventEmitter);	
 
 //=============================================================================
 
-
+/*
+ * state handler
+ * opens connection, grabs packets, store complete system state
+ */
 
 exports.PKT_ACK = PKT_ACK;
 exports.PKT_NACK = PKT_NACK;
-exports.engine = myHomeEngine;
+exports.engine = myHomeLayer2;
