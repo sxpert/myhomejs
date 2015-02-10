2d334a55cab4ccdbf8eec9b4b972295d538bb2e8
Michael Grant <mgrant@grant.org>
2015-02-10 13:22:00 +0000

    added some functions to get status of heating system and set temp and mode

--- myhome.js
+++ myhome.js
@@ -241,6 +241,7 @@ var myHomeLayer2 = function (params) {
 	this.monitor.on('packet', function (data) {
 		self.emit ('packet', data);
 	});
+
 };
 util.inherits(myHomeLayer2, events.EventEmitter);	
 
@@ -326,6 +327,197 @@ util.inherits(myHomeLayer2, events.EventEmitter);
 	this.scanConfigured = function (log, callback) {
 		_scanSystem({log: log, cmd: '*#1001*0*13#1##', done: callback});
 	}
+	/*
+	 * callable system config functions
+	 */
+
+	this.getZones = function (callback) {
+	/* NOTE: I was hoping this would return a list of zones but it does not work for me
+	 *getZones calls callback(zones, success) function:
+	 * zones is an object of zones
+	 * success is 1: ack, 2: nack
+	 */
+		var zones = {};
+		this.sendCommand({
+			/* command:'*#1004*#0*21##', */
+			command:'*#4*001*0##',
+                      	stopon: ['*#*1##', '*#*0##'],
+			packet: function(cc,pkt) {
+console.log('got:'+pkt);
+				var m;
+				m = pkt.match(/^\*#1004\*#(\d+)\*21\*(\d*)##/);
+				if (m!==null) {
+					/* Zone operation temperature not adjust by local offset.
+					 * m[1] = zone
+					 * m[2] = BITS
+					 *  BIT field is composed by BIT1 ... BIT16. The most important bits are:
+					 *    Bit11 = 0 a probe does not answer
+					 *    Bit12 = 0 a pump does not answer
+					 *    Bit13 = 0 EEPROM read/write failure
+					 *    Bit14 = 0 temperature out of range
+					 *    Bit15 = 0 a slave probe does not answer
+					 *    Bit16 = 0 an actuator does not answer
+					 */
+					zones[m[1]] = m[2];
+					return;
+				}
+			},
+			done: function(pkt,i) {
+				var m = pkt.match(/\*#\*(\d)##/);
+				var successFail= m[1]; /* 1 = ack, 0 = nack */
+				callback(zones, successFail);
+			}
+                });
+	}
+
+	this.getStatus = function (zone, callback) {
+	/* getStatus calls callback(status, success) function:
+	 * status is like this:
+	 * { opTemp: '0185',
+	 *   opTempWithOffset: '0190',
+	 *   opMode: '1',
+	 *   setPointTemp: '0190',
+	 *   offsetKnob: '00' }
+	 * success is 1: ack, 2: nack
+	 */
+		var statusResponse = {};
+		this.sendCommand({
+			command:'*#4*'+zone+'##', /* *#4*where## */
+                      	stopon: ['*#*1##', '*#*0##'],
+			packet: function(cc,pkt) {
+				var m;
+				m = pkt.match(/^\*#4\*(\d+)\*0\*(\d+)##/);
+				if (m!==null) {
+					/* Zone operation temperature not adjust by local offset.
+					 * m[1] = zone
+					 * m[2] = 0000 to 0500 degrees in .1 degree
+					 */
+					statusResponse.opTemp = m[2];
+					return;
+				}
+				m = pkt.match(/^\*#4\*(\d+)\*12\*(\d+)\*3##/);
+				if (m!==null) {
+					/* Zone operation temperature with adjust by local offset
+					 * m[1] = zone
+                                         * m[2] = 0020 to 0430 degrees in .1 degree
+                                         */
+					statusResponse.opTempWithOffset = m[2];
+					return;
+				}
+				m = pkt.match(/^\*4\*(\d+)\*(\d+)##/);
+				if (m!==null) {
+					/* Zone operation mode
+					 * m[2] = zone
+                                         * m[1] = mode
+					 *   0 Conditioning
+					 *   1 Heating
+					 *   102 Antifreeze
+					 *   202 Thermal Protection
+					 *   303 Generic OFF
+                                         */
+					statusResponse.opMode = m[1];
+					return;
+				}
+				m = pkt.match(/^\*#4\*(\d+)\*13\*(\d+)##/);
+				if (m!==null) {
+					/* Zone local offset
+					 * m[1] = zone
+                                         * m[2] = offset
+					 *   00 knob on 0
+					 *   01 knob on +1 (degree)
+					 *   11 knob on -1 (degree)
+					 *   02 knob on +2 (degree)
+					 *   12 knob on -2 (degree)
+					 *   03 knob on +3 (degree)
+					 *   13 knob on -3 (degree)
+					 *   4 knob on Local OFF
+					 *   5 knob on Local protection
+                                         */
+					statusResponse.offsetKnob = m[2];
+					return;
+				}
+				m = pkt.match(/^\*#4\*(\d+)\*14\*(\d+)\*3##/);
+				if (m!==null) {
+					/* Zone set point temperature
+					 * m[1] = zone
+					 * m[2] = 0050 to 0400 degrees in .1 degree
+					 */
+					statusResponse.setPointTemp = m[2];
+					return;
+				}
+				
+			},
+			done: function(pkt,i) {
+				var m = pkt.match(/\*#\*(\d)##/);
+				var successFail= m[1]; /* 1 = ack, 0 = nack */
+				callback(statusResponse, successFail);
+			}
+                });
+	}
+	
+	this.setTemp = function (temp) {
+		this.sendCommand({
+			/* Manual setting of “N” zone to T temperature
+			 * *#4*where*#14*T*M##
+			 * T = 0050 to 0400
+			 * M = 1 heating
+			 *     2 air-conditioning
+			 *     3 generic
+			 */
+			command:'*#4*1*#14*'+temp+'*1##', 
+                      	stopon: ['*#*1##', '*#*0##'],
+			done: function(pkt,i) {
+				var m = pkt.match(/\*#\*(\d)##/);
+				var successFail = m[1];
+				callback(statusResponse, successFail);
+			}
+                });
+	}
+	
+	this.setModeOff = function () {
+		this.sendCommand({
+			/* Set the “N” zone in off mode
+			 * *4*303*where##
+			 */
+			command:'*4*303*1##', 
+                      	stopon: ['*#*1##', '*#*0##'],
+			done: function(pkt,i) {
+				var m = pkt.match(/\*#\*(\d)##/);
+				var successFail = m[1];
+				callback(statusResponse, successFail);
+			}
+                });
+	}
+	
+	this.setModeAntifreeze = function () {
+		this.sendCommand({
+			/* Set the “N” zone in antifreeze mode
+			 * *4*102*where##
+			 */
+			command:'*4*102*1##', 
+                      	stopon: ['*#*1##', '*#*0##'],
+			done: function(pkt,i) {
+				var m = pkt.match(/\*#\*(\d)##/);
+				var successFail = m[1];
+				callback(statusResponse, successFail);
+			}
+                });
+	}
+	
+	this.setModeAuto = function () {
+		this.sendCommand({
+			/* Set the “N” zone in automatic mode
+			 * *4*311*where##
+			 */
+			command:'*4*311*#1##', 
+                      	stopon: ['*#*1##', '*#*0##'],
+			done: function(pkt,i) {
+				var m = pkt.match(/\*#\*(\d)##/);
+				var successFail = m[1];
+				callback(statusResponse, successFail);
+			}
+                });
+	}
 	
 	//============================================================================
 	
@@ -395,3 +587,4 @@ exports.MODE_CONFIG = MODE_CONFIG;
 exports.PKT_ACK = PKT_ACK;
 exports.PKT_NACK = PKT_NACK;
 exports.engine = myHomeLayer3;
+
